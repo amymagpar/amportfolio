39931aaed5e25c809d73488c31e798a87378aaf0
arielchuri <github@sparklelabs.com>
2025-02-13 17:48:11 -0500

    script changes

--- amy_portfolio_25_a/site/static/js/combined.js
+++ amy_portfolio_25_a/site/static/js/combined.js
@@ -6,9 +6,6 @@ document.addEventListener("DOMContentLoaded", () => {
   <title>A. Parness | Home</title>
 
 
-    <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=yes" />
-  <meta http-equiv="x-ua-compatible" content="ie=edge" />
 
 <link rel="stylesheet" href="https://www.gstatic.com/dialogflow-console/fast/df-messenger/prod/v1/themes/df-messenger-default.css">
 <style>
